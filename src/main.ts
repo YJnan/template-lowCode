ce1c8a939edfe06b7d8e8061d738b937eda193e0
yuanjiannan01 <il>
2021-06-17 09:03:32 +0800

    init style

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from './router/index'
 import vuex from './store/index'
 
+
 const  app = createApp(App)
 
-app.use(router)
-app.use(vuex)
-app.mount('#app')
+// app.config.globalProperties.$$refs = {}
+
+// // if (import.meta.env.DEV) {
+// window.$$refs = app.config.globalProperties.$$refs
+// }
+app.use(router).use(vuex)
+// 路由准备完毕再挂载
+router.isReady().then(() => app.mount('#app'))
